8cbbb7bc4875c9b5b9f3665f2eff585d14833069
Nishanth1205-web <nishantharasu1205@gmail.com>
2026-09-18 09:57:02 +0530

    Connect frontend to Render API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+ axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL || 'https://to-do-backend-ud8g.onrender.com';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
